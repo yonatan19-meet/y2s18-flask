8f0356ca41e514455697a0c82373e0682dd880ff
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 18:10:30 +0300

    hey!

--- exercises/app.py
+++ exercises/app.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
+    # players = ["Asi Abu", "Ben Boy", "Cinnamon Comfortable", "Dori Debest"]
     return render_template("index.html")
+    # , players=players, like_same_sport=True)
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    app.run(debug = True)
 
 
-
-   
